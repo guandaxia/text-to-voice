77dc5f94298ef7b0ddddcf8fcb372a3c6517538d
guansixu <guansixu@gmail.com>
2017-08-14 11:38:19 +0800

    修改配置文件

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ let userHome = app.getPath('home')
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 1000, height: 800 })
+  mainWindow = new BrowserWindow({ width: 500, height: 800 })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
